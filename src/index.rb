6fc3428e80a75d0b0e0ac2db0c90e8c529a3e2d8
Mitchell Boyle <mitchellboyle97@gmail.com>
2021-05-27 21:25:37 +1000

    Moved user input into a method. Added input validation using regex

--- src/index.rb
+++ src/index.rb
@@ -8,29 +8,21 @@ prompt = TTY::Prompt.new
 system "clear"
 app_title
 
-#input for player 1
-puts "Enter player one name: "
-player1_name = gets.chomp
-puts "Enter player one army: "
-player1_army = gets.chomp
-puts "Enter player one command points: "
-player1_cp = gets.chomp
-player1_pp = 0
-player1_sp= 0
+@player1_name = ""
+@player1_army = ""
+@player1_pp = 0
+@player1_sp= 0
+@player1_cp = 0
+player1_input @player1_name, @player1_army, @player1_cp
+player1 = Player.new(@player1_name, @player1_army, @player1_pp, @player1_sp, @player1_cp)
 
-player1 = Player.new(player1_name, player1_army, player1_pp, player1_sp, player1_cp)
-
-#input for player 2
-puts "Enter player two name: "
-player2_name = gets.chomp
-puts "Enter player two army: "
-player2_army = gets.chomp
-puts "Enter player two command points: "
-player2_cp = gets.chomp
-player2_pp = 0
-player2_sp = 0
-
-player2 = Player.new(player2_name, player2_army, player2_pp, player2_sp, player2_cp)
+@player2_name = ""
+@player2_army = ""
+@player2_pp = 0
+@player2_sp= 0
+@player2_cp = 0
+player2_input @player2_name, @player2_army, @player2_cp
+player2 = Player.new(@player2_name, @player2_army, @player2_pp, @player2_sp, @player2_cp)
 
 system "clear"
 app_title
@@ -64,7 +56,7 @@ case(menu_input)
         puts ""
         puts ""
     when 2
-        puts "How many command points did #{player1.name} spend: "
+        puts "How many command points did #{player1.name} spend this turn: "
         player1.cmd_points = player1.cmd_points - gets.chomp.to_i
         system "clear"
         app_title
@@ -88,7 +80,7 @@ case(menu_input)
         puts ""
         puts ""
     when 4
-        puts "How many command points did #{player2.name} spend: "
+        puts "How many command points did #{player2.name} spend this turn: "
         player2.cmd_points = player2.cmd_points - gets.chomp.to_i
         system "clear"
         app_title
--- src/methods.rb
+++ src/methods.rb
@@ -15,3 +15,43 @@ def app_title
     puts ""
     puts ""
 end
+
+def player1_input player1_name, player1_army, player1_cp
+    while true
+        puts "Enter player one name: "
+        @player1_name = gets.chomp
+        puts "Enter player one army: "
+        @player1_army = gets.chomp
+        while @player1_army.match /^[0-9]+$/
+            puts "Please enter an valid army"
+            @player1_army = gets.chomp
+        end
+        puts "Enter player one command points: "
+        @player1_cp = gets.chomp
+        while @player1_cp.match /^[a-zA-Z]+$/
+            puts "Please enter a number"
+            @player1_cp = gets.chomp
+        end
+        break
+    end
+end
+
+def player2_input player2_name, player2_army, player2_cp
+    while true
+        puts "Enter player two name: "
+        @player2_name = gets.chomp
+        puts "Enter player two army: "
+        @player2_army = gets.chomp
+        while @player2_army.match /^[0-9]+$/
+            puts "Please enter an valid army"
+            @player2_army = gets.chomp
+        end
+        puts "Enter player two command points: "
+        @player2_cp = gets.chomp
+        while @player2_cp.match /^[a-zA-Z]+$/
+            puts "Please enter a number"
+            @player2_cp = gets.chomp
+        end
+        break
+    end
+end
